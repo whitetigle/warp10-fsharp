a2c3e69adba5fdf93249d7ad9427e3d48aeb89ad
francois nicaise <whitetigle@gmail.com>
2018-09-12 10:45:34 +0200

    1.0.0-beta-001

--- src/Warp10/RELEASE_NOTES.md
+++ src/Warp10/RELEASE_NOTES.md
@@ -1,4 +1,14 @@
 
+#### 1.0.0-beta-001
+
+* All major endpoints are ready and fully typed
+* WarpScript:Fetch available
+* WarpScript:Bucketize + 100% bucketizer operations available
+* WarpScript:Filter + 100% filter operations available
+* Map (WIP)
+* Reduce (WIP)
+* Operations can be piped
+
 #### 0.0.9
 
 * Initial release
--- tests/Tests.Warp10.Samples.fs
+++ tests/Tests.Warp10.Samples.fs
@@ -8,6 +8,21 @@ open Util
 open Util.TestsWithPromises
 open Warp10.Shared.DateUtils
 
+(*
+[
+    'sMewcQeIFrr2v2JTCHCGTqMRDr2Ubb7lxlUFZ6Ft6isAJQxZIQUwA0I038J7X9uBy6b6akOlRQYeW1N_35cMusYJMB7NuKP79sz.kK5PwxBkQDtHBT4Ls6oODTe19DerIH047n7weB_rDxGcdHIv3L.Tg8GJLMcC'
+    'speed'
+    {
+    }
+    NOW -1000000000000
+]
+FETCH
+[ SWAP bucketizer.count 0 300000000 0 ] BUCKETIZE
+<%
+  DROP DUP TICKS <% DROP 'UTC' ISO8601 %> LMAP SWAP VALUES 2 ->LIST ZIP
+%> LMAP
+*)
+
 describe "Warp10.Samples" <| fun _ ->
 
     let test (expected:string) (script:string) =
@@ -24,7 +39,7 @@ describe "Warp10.Samples" <| fun _ ->
 
     describe "Sensor" <| fun _ ->
 
-        it "should get data for  sensor" <| fun _ ->
+        it "should get last value for card #39 and sensor #4" <| fun _ ->
             let expected = """{"c":"myarduino.sensor4","l":{"id":"39",".app":"test","kind":"psi"},"a":{},"v":[[1534348800000000,99]]}"""
             let expected = sprintf "[[%s]]" expected
             let labels = None
@@ -33,5 +48,41 @@ describe "Warp10.Samples" <| fun _ ->
             (script,[])
                 |> GTS.fetch readToken "~myarduino.*" ["id","39"] 1534348800000000. -1.
                 |> GTS.filter labels (Filter.ByClass "~myarduino.sensor4")
+                |> GTS.join
+                |> test expected
+
+        it "should get max temperature for all temp sensors on card #1" <| fun _ ->
+            let expected = """{"c":"myarduino.sensor2","l":{"id":"1",".app":"test","kind":"temp"},"a":{},"v":[[1534348800000000,99]]},{"c":"myarduino.sensor1","l":{"id":"1",".app":"test","kind":"temp"},"a":{},"v":[[1534348800000000,99]]}"""
+            let expected = sprintf "[[%s]]" expected
+            let labels = None
+
+            let script = ""
+            (script,[])
+                |> GTS.fetch readToken "~myarduino.*" ["kind","temp";"id","1"] 1534348800000000. -10000000000.
+                |> GTS.bucketize Bucketizer.Max 0 0 1
+                |> GTS.join
+                |> test expected
+
+        it "should get max temperature for temp sensor #2 on card #1" <| fun _ ->
+            let expected = """{"c":"myarduino.sensor2","l":{"id":"1",".app":"test","kind":"temp"},"a":{},"v":[[1534348800000000,99]]}"""
+            let expected = sprintf "[[%s]]" expected
+            let labels = None
+
+            let script = ""
+            (script,[])
+                |> GTS.fetch readToken "myarduino.sensor2" ["kind","temp";"id","1"] 1534348800000000. -10000000000.
+                |> GTS.bucketize Bucketizer.Max 0 0 1
+                |> GTS.join
+                |> test expected
+
+        it "should get max temperature for temp sensor #2 on card #1 for the last hour" <| fun _ ->
+            let expected = """{"c":"myarduino.sensor2","l":{"id":"1",".app":"test","kind":"temp"},"a":{},"v":[[1534348800000000,99]]}"""
+            let expected = sprintf "[[%s]]" expected
+            let labels = None
+
+            let script = ""
+            (script,[])
+                |> GTS.fetch readToken "myarduino.sensor2" ["kind","temp";"id","1"] 1534348800000000. 36000000000.
+                |> GTS.bucketize Bucketizer.Max 0 0 1
                 |> GTS.join
                 |> test expected
